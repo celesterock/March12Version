97729fff805244b87292d30c1619b94e433dbf83
Daniboss101 <136389310+Daniboss101@users.noreply.github.com>
2024-03-12 23:03:39 -0700

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -9,40 +9,70 @@
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 QT_END_NAMESPACE
 
+/**
+ * @brief The MainWindow class represents the main application window.
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor for the MainWindow class.
+     * @param dbManager Pointer to the DbManager instance.
+     * @param parent The parent widget (default is nullptr).
+     */
     explicit MainWindow(DbManager* dbManager, QWidget *parent = nullptr);
+
+    /**
+     * @brief Destructor for the MainWindow class.
+     */
     ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
-    DbManager* dbManager; // pointer to DbManager instance
-    CollegeList *collegeListWindow = nullptr; // pointer to the college list window
-    adminDialog* newAdminDialog;
+    Ui::MainWindow *ui; ///< Pointer to the UI object for the MainWindow.
 
-    QMenu* administrativeAccess;
+    DbManager* dbManager; ///< Pointer to the DbManager instance.
 
-    QAction* toLoginAct;
+    CollegeList *collegeListWindow = nullptr; ///< Pointer to the college list window.
 
+    adminDialog* newAdminDialog; ///< Pointer to the admin dialog window.
 
+    QMenu* administrativeAccess; ///< Menu for administrative access.
+
+    QAction* toLoginAct; ///< Action to switch to login view.
+
+    /**
+     * @brief Creates menus for the MainWindow.
+     */
     void createMenus();
+
+    /**
+     * @brief Creates actions for the MainWindow.
+     */
     void createActions();
 
-    // New functions 3/12
+    /**
+     * @brief Creates the correct trip order for the trip planner.
+     * @param trip Pointer to the TripPlanner instance.
+     */
     void createCorrectTripOrder(TripPlanner* trip);
-
 
 private slots:
     // void openCollegeListWindow();
+    /**
+     * @brief Slot to begin the trip.
+     */
     void beginTrip();
-    void toLogin();
 
+    /**
+     * @brief Slot to switch to login view.
+     */
+    void toLogin();
 };
+
 #endif // MAINWINDOW_H
