7a0d8ded2308cdc41c44c10b0b66e617e8c8de8c
Daniboss101 <136389310+Daniboss101@users.noreply.github.com>
2024-03-12 23:06:20 -0700

    Update visitcollegewindow.h

--- visitcollegewindow.h
+++ visitcollegewindow.h
@@ -9,28 +9,46 @@ namespace Ui {
 class VisitCollegeWindow;
 }
 
+/**
+ * @brief The VisitCollegeWindow class represents the window for visiting a college.
+ */
 class VisitCollegeWindow : public QDialog
 {
     Q_OBJECT
 
 public:
-    /* points to the Trip instance so that it can update the souvenirs
-     * purchased / total cost for each college that has been visited */
+    /**
+     * @brief Constructor for the VisitCollegeWindow class.
+     * @param trip Pointer to the TripPlanner instance.
+     * @param parent The parent widget.
+     */
     explicit VisitCollegeWindow(TripPlanner* trip, QWidget *parent = nullptr);
+
+    /**
+     * @brief Destructor for the VisitCollegeWindow class.
+     */
     ~VisitCollegeWindow();
 
-    // Sets the college name that will be visited
+    /**
+     * @brief Sets the college to be visited.
+     * @param collegeToVisit The college to be visited.
+     */
     void setCollege(const College& collegeToVisit);
 
-    // Puts the souvenirs & prices into the table
+    /**
+     * @brief Populates the souvenir table with souvenirs and prices.
+     * @param collegeToVisit The college to visit.
+     */
     void populateSouvenirTable(const College& collegeToVisit);
-
 
 private:
     Ui::VisitCollegeWindow *ui;
     TripPlanner* trip;
 
 private slots:
+    /**
+     * @brief Slot to handle the purchase button click event.
+     */
     void purchaseButtonClick();
 
 };
