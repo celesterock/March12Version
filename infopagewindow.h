91fb9ec445ce31fca05c917a796a5bcb387a2026
Daniboss101 <136389310+Daniboss101@users.noreply.github.com>
2024-03-12 23:02:40 -0700

    Update infopagewindow.h

--- infopagewindow.h
+++ infopagewindow.h
@@ -4,19 +4,30 @@
 #include <QDialog>
 
 namespace Ui {
-class InfoPageWindow;
+    class InfoPageWindow;
 }
 
+/**
+ * @brief The InfoPageWindow class represents a dialog window to display information.
+ */
 class InfoPageWindow : public QDialog
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Explicit constructor for the InfoPageWindow class.
+     * @param parent The parent widget (default is nullptr).
+     */
     explicit InfoPageWindow(QWidget *parent = nullptr);
+
+    /**
+     * @brief Destructor for the InfoPageWindow class.
+     */
     ~InfoPageWindow();
 
 private:
-    Ui::InfoPageWindow *ui;
+    Ui::InfoPageWindow *ui; ///< Pointer to the UI object for the InfoPageWindow dialog window.
 };
 
 #endif // INFOPAGEWINDOW_H
