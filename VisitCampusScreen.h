5c9b43a24fe4b7f1df6e547fb087175b862c9fd8
Daniboss101 <136389310+Daniboss101@users.noreply.github.com>
2024-03-12 22:56:09 -0700

    Update VisitCampusScreen.h

--- VisitCampusScreen.h
+++ VisitCampusScreen.h
@@ -7,16 +7,27 @@ namespace Ui {
 class InfoPageWindow;
 }
 
+/**
+ * @brief The InfoPageWindow class represents a dialog for displaying information about visiting a college campus.
+ */
 class InfoPageWindow : public QDialog
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructs an InfoPageWindow object.
+     * @param parent Pointer to the parent widget.
+     */
     explicit InfoPageWindow(QWidget *parent = nullptr);
+
+    /**
+     * @brief Destructor for the InfoPageWindow object.
+     */
     ~InfoPageWindow();
 
 private:
-    Ui::InfoPageWindow *ui;
+    Ui::InfoPageWindow *ui; ///< Pointer to the UI object
 };
 
 #endif // VISITCAMPUSSCREEN_H
